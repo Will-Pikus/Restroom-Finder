709d197f5317a4c40786368c9fc62c2fd933c535
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-01-26 16:22:22 -0800

    Basic functions console logging

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ let where = document.getElementById('location')
 let gender = document.getElementById('gender-dropdown')
 let submit = document.getElementById('submitbtn')
 
+
+
 submit.addEventListener('click', function() { 
     console.log(distance.value);
     console.log(public.value);
@@ -27,6 +29,37 @@ function getLocation() {
   }
   
   function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+    console.log( "Latitude: " + position.coords.latitude +
+    "Longitude: " + position.coords.longitude);
   }
+//This function returns ONE result.
+  function query() {
+
+    fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mcdonalds%20san%20diego&inputtype=textquery&fields=formatted_address&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);
+     
+    })
+  }
+  //This one searches for items matching the keyword around the same area.
+  function query2() {
+
+    fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.7547401,-117.1414917&radius=50000&keyword=starbucks&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
+    .then(response => response.json())
+    .then(data => {
+      let results = data['results'][0].vicinity;
+      
+      console.log(data);
+      console.log(results);
+
+    })
+  }
+  //This link will search for nearby places
+  //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=[38.8976763%2C-77.0365298]&radius=50000&keyword=mcdonalds&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM
+  //Places API takes text input. Our text input will always be the same. We just
+//function to use the Places API
+//add event listener to search. ask for location upon search.
+//pass location search as the argument for API call
+//Function to make divs populated with search results
+//just update the count of restroom users.
