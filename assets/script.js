9b806fc48dec84b930ddcf99a4de84fe4385cba4
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-01-27 18:40:57 -0800

    fixed a function

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ let public = document.getElementById('public-dropdown')
 let where = document.getElementById('location')
 let gender = document.getElementById('gender-dropdown')
 let submit = document.getElementById('submitbtn')
-
+let lat;
+let lng;
 
 
 submit.addEventListener('click', function() { 
@@ -16,57 +17,6 @@ submit.addEventListener('click', function() {
 let map, infoWindow;
 
 function initMap() {
-<<<<<<< HEAD
-    map = new google.maps.Map(document.getElementById('map'),{
-        center: {lat: 66, lng: 66},
-        zoom: 8,
-        mapId: 'e5c1493f9a045ddd'
-    })
-}
-function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
-  
-  function showPosition(position) {
-    console.log( "Latitude: " + position.coords.latitude +
-    "Longitude: " + position.coords.longitude);
-  }
-//This function returns ONE result.
-  function query() {
-
-    fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mcdonalds%20san%20diego&inputtype=textquery&fields=formatted_address&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
-    .then(response => response.json())
-    .then(data => {
-    console.log(data);
-     
-    })
-  }
-  //This one searches for items matching the keyword around the same area.
-  function query2() {
-
-    fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.7547401,-117.1414917&radius=50000&keyword=starbucks&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
-    .then(response => response.json())
-    .then(data => {
-      let results = data['results'][0].vicinity;
-      
-      console.log(data);
-      console.log(results);
-
-    })
-  }
-  //This link will search for nearby places
-  //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=[38.8976763%2C-77.0365298]&radius=50000&keyword=mcdonalds&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM
-  //Places API takes text input. Our text input will always be the same. We just
-//function to use the Places API
-//add event listener to search. ask for location upon search.
-//pass location search as the argument for API call
-//Function to make divs populated with search results
-//just update the count of restroom users.
-=======
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 15,
@@ -87,7 +37,9 @@ function getLocation() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
-
+          lat = position.coords.latitude;
+          lng = position.coords.longitude;
+          console.log(lat)
           infoWindow.setPosition(pos);
           map.setCenter(pos);
 
@@ -98,11 +50,13 @@ function getLocation() {
               url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
             animation: google.maps.Animation.DROP,
           });
+          
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
         }
       );
+
     } else {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
@@ -120,4 +74,36 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
->>>>>>> 15532ab5d903a16025fb5876270aeeb29bd5471d
+
+
+function query() {
+
+  fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mcdonalds%20san%20diego&inputtype=textquery&fields=formatted_address&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
+  .then(response => response.json())
+  .then(data => {
+  console.log(data);
+   
+  })
+}
+//This one searches for items matching the keyword around the same area.
+function query2() {
+console.log(lat);
+  fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ lat+ '%2' + lng +'&radius=50000&keyword=starbucks&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let results = data['results'][0].vicinity;
+    
+    console.log(data);
+    console.log(results);
+
+  })
+}
+//This link will search for nearby places
+//https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.8976763%2C-77.0365298&radius=50000&keyword=mcdonalds&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM
+//Places API takes text input. Our text input will always be the same. We just
+//function to use the Places API
+//add event listener to search. ask for location upon search.
+//pass location search as the argument for API call
+//Function to make divs populated with search results
+//just update the count of restroom users.
