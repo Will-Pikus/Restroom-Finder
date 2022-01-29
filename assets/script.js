30487f97dff1d453b731b8078880943f3b1fa0fe
Robert Bullfrog <williampikus@gmail.com>
2022-01-29 10:40:46 -0800

    Created directions function, parsed code creating functions for init map and get current location

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,9 @@ let gender = document.getElementById('gender-dropdown')
 let submit = document.getElementById('submitbtn')
 let lat;
 let lng;
-
+var myLatLng = {lat: 38.3460, lng: -0.4907}
+var dest = "New York, NY, USA"
+var start = "Las Vegas, NV, USA"
 
 submit.addEventListener('click', function() { 
     console.log(distance.value);
@@ -16,19 +18,69 @@ submit.addEventListener('click', function() {
 
 let map, infoWindow;
 
+// INITIATE MAP FUNCTION
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 15,
+  var mapOptions = {
+      center: myLatLng,
+      zoom: 7,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  //create map
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var currentLoc = getCurrLoc(map)
+  //create a DirectionsService object to use the route method and get a result for our request
+  var directionsService = new google.maps.DirectionsService();
+
+  //create a DirectionsRenderer object which we will use to display the route
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+
+  //bind the DirectionsRenderer to the map
+  directionsDisplay.setMap(map);
+  calcRoute(directionsService,directionsDisplay,currentLoc,map)
+}
+
+// CALCULATE ROUTE FUNCTION
+function calcRoute(directionsService,directionsDisplay,currentLoc, map) {
+  //create request
+  var request = {
+      origin: start,
+      destination: dest,
+      travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+      unitSystem: google.maps.UnitSystem.IMPERIAL
+  }
+
+  //pass the request to the route method
+  directionsService.route(request, function (result, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+
+          //Get distance and time and display on DOM
+          // const output = document.querySelector('#output');
+          // output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+
+          //display route
+          directionsDisplay.setDirections(result);
+      } else {
+          //delete route from map
+          directionsDisplay.setDirections({ routes: [] });
+          //center map in London
+          map.setCenter(myLatLng);
+
+          //show error message
+          // output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+      }
   });
+}
+
+function getCurrLoc(map){
   infoWindow = new google.maps.InfoWindow();
+  
+  // const locationButton = document.createElement("button");
+  // locationButton.textContent = "Pan to Current Location";
+  // locationButton.classList.add("custom-map-control-button");
+  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  // locationButton.addEventListener("click", () => {
 
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -37,11 +89,10 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
-          lat = position.coords.latitude;
-          lng = position.coords.longitude;
-          console.log(lat)
+
           infoWindow.setPosition(pos);
           map.setCenter(pos);
+          // return pos
 
           const marker = new google.maps.Marker({
             position: pos,
@@ -50,31 +101,33 @@ function initMap() {
               url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
             animation: google.maps.Animation.DROP,
           });
-          
+          return pos
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
         }
       );
-
-    } else {
+    } 
+    else {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
- 
     }
-  });
+  // });
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
 
+initMap()
+ 
 
 function query() {
 
