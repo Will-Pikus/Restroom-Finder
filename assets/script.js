8dcd84abecd5d92ce71d4103619503c1c5b495c2
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-01-29 10:30:51 -0800

    Cleaned up code a little

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,17 @@ let public = document.getElementById('public-dropdown')
 let where = document.getElementById('location')
 let gender = document.getElementById('gender-dropdown')
 let submit = document.getElementById('submitbtn')
-let submit2 = document.getElementById('submitbtn2')
 let resultsdiv = document.getElementById('result-container')
-let keyword = "(mcdonalds) | (starbucks) | (wal-mart)"
 let lat;
 let lng;
 
-
-submit.addEventListener('click', function() { 
-
-});
+//This is our input query. Need to pass it an "And/Or" instead of just the OR it currently has. Havent figured out the exact syntax, worked once, but i forget how.
+let keyword = "(mcdonalds) |& (starbucks) | (wal-mart)"
 
 let map, infoWindow;
 
 const locationButton = document.createElement("button");
-
+//Map Function
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
@@ -31,7 +27,48 @@ function initMap() {
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
  
 }
+
 function getGeoLocation(){
+
+
+  // Try HTML5 geolocation.
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        
+         lat = position.coords.latitude;
+         lng = position.coords.longitude;
+
+   
+        infoWindow.setPosition(pos);
+        map.setCenter(pos);
+
+        const marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          icon: {                             
+            url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"},
+          animation: google.maps.Animation.DROP,
+        });
+   
+        
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+
+
+
+
+}
+//Same function as previous BUT it calls the query2function, so it populates the DOM with results.
+function getGeoLocation2(){
 
 
   // Try HTML5 geolocation.
@@ -72,7 +109,6 @@ function getGeoLocation(){
 
 
 }
-
 locationButton.addEventListener("click", () => {
 getGeoLocation();
 });
@@ -88,15 +124,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 }
 
 
-// function query() {
 
-//   fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mcdonalds%20san%20diego&inputtype=textquery&fields=formatted_address&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
-//   .then(response => response.json())
-//   .then(data => {
-//   console.log(data);
-   
-//   })
-// }
 
 //This one searches for items matching the keyword around the same area.
 function query2(test, test2) {
@@ -122,10 +150,19 @@ function query2(test, test2) {
   })
 };
 
-submit2.addEventListener('click', async function() {
+submit.addEventListener('click', function() {
 
-   getGeoLocation();
-  // console.log(lat, lng)
-  // query2(lat, lng);
+   getGeoLocation2();
+
 });
 
+//Dont want to delete this function just in case we end up wanting to do a multi-query thing or something.
+// function query() {
+
+//   fetch('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mcdonalds%20san%20diego&inputtype=textquery&fields=formatted_address&key=AIzaSyCItHTXTMZs3fcjRKsg7UcaNeWLUdTIdDM')
+//   .then(response => response.json())
+//   .then(data => {
+//   console.log(data);
+   
+//   })
+// }
